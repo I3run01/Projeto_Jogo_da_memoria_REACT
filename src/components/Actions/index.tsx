947281eef61b9aa1ae0cbe7e28583f8e2e3c8753
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-07-19 20:36:50 -0300

    c

--- src/components/Actions/index.tsx
+++ src/components/Actions/index.tsx
@@ -1,9 +1,34 @@
 import * as s from "./style"
+import { useState } from "react";
+
 
 export const Actions = () => {
+    
+    const StartTime = () => {
+        setTimeout(() => {
+            
+        }, 1000);
+    }
+
     return (
         <s.ActionsDiv>
-            <h1>ActionsDiv</h1>
+            <div id="MenuDiv">
+                <div id="Time">
+                    <h1>Time</h1>
+                    <p>00:00</p>
+                </div>
+                <div id="Movements">
+                    <h1>Movements</h1>
+                    <p>0</p>
+                </div>
+            </div>
+            <div id="ButtonDiv" onClick={StartTime}>
+                <div id="BTN">
+                    <div id="BtnSymbol"></div>
+                    <div id="BtnRestart">Restart</div>
+                </div>
+                
+            </div>
         </s.ActionsDiv>
     )
 }
--- src/components/Actions/style.ts
+++ src/components/Actions/style.ts
@@ -1,5 +1,61 @@
 import styled from "styled-components";
 
 export const ActionsDiv = styled.div`
-    grid-area: 'ActionsDiv';
+    #MenuDiv {
+        display: flex;
+        justify-content: space-around;
+
+        h1 {
+            font-size: 1em;
+            text-align: center;
+            font-weight: lighter;
+            
+        }
+
+        p {
+            font-size: 2.5em;
+            text-align: center;
+            margin: 0;
+            font-weight: bold;
+        }
+    }
+
+    #ButtonDiv {
+        display: flex;
+        justify-content:center;
+
+        
+
+        #BTN {
+            display: flex;
+            flex-direction: row;
+            margin-top: 30px;
+            min-height: 40px;
+            text-align: center;
+            color: white;
+            font-size: 1.4em;
+            cursor: pointer;
+            border-radius: 20px 0 0 20px;
+
+        }
+
+        #BtnSymbol, #BtnRestart {
+            background-color: blue;
+            padding: 20px 0 20px 0;
+        }
+
+        #BtnSymbol {
+            min-width: 20px;
+            border-radius: 20px 0 0 20px;;
+        }
+
+        #BtnRestart {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            min-width: 120px;
+            border-radius: 0 20px 20px 0;
+        }
+    }
+
 `
