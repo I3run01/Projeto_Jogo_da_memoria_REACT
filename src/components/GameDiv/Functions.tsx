ed548290f898e87cd23b5752fa887f42a190fed6
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-07-24 17:25:25 -0300

    c

--- src/components/GameDiv/Functions.tsx
+++ src/components/GameDiv/Functions.tsx
@@ -49,6 +49,7 @@ export const Reset = () => {
         item.CardInTheGame = true
         item.OpenCard = false
         hideCard(item.Id)
+        numClosedsCardsOBJ.numberClosedsCards = 0
     })
 }
 
